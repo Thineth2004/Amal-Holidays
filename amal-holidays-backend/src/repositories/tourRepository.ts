7db66d5e70df4623e3b731770efa9c502058bcf4
Thineth <thinethgeewinda660@gmail.com>
2026-03-27 21:49:24 +0530

    feat(amal-holidays-backend/src/repositories): define the DB layer for a tour package

---
+++ amal-holidays-backend/src/repositories/tourRepository.ts
@@ -0,0 +1,49 @@
+import { pool } from "../config/db";
+
+// Create Tour
+export const createTour = async (data: any) => {
+    const {
+        title,
+        description,
+        price,
+        capacity,
+        available_seats,
+        start_date,
+        end_date,
+    } = data;
+
+    const result = await pool.query(
+        `INSERT INTO tour_package
+        (title, description, duration, price, capacity, available_seats, start_date, end_date)
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8)
+        RETURNING *`,
+        [title, description, price, capacity, available_seats, start_date, end_date]
+    );
+
+    return result.rows[0];
+};
+
+// Get all tours
+export const getAllTours = async () => {
+    const result = await pool.query(
+        `SELECT * FROM tour_package ORDER BY start_date ASC`
+    );
+    return result.rows;
+};
+
+// Get available tours
+export const getAvailableTours = async () => {
+    const result = await pool.query(
+        `SELECT * FROM tour_package WHERE available_slots > 0`
+    );
+    return result.rows;
+};
+
+// Get single tour 
+export const getTourPackageById = async (id: number) => {
+    const result = await pool.query(
+        `SELECT * FROM tour_package WHERE package_id = $1`,
+        [id]
+    );
+    return result.rows[0];
+};
