411b5534723a5a2eee4bcf0aa5e7bf9084ca6509
Thineth <thinethgeewinda660@gmail.com>
2026-04-21 23:19:53 +0530

    feat(amal-holidays-backend/src/repositories): add the location and destination attributes to the sql query

--- amal-holidays-backend/src/repositories/destinationRepository.ts
+++ amal-holidays-backend/src/repositories/destinationRepository.ts
@@ -1,14 +1,14 @@
 import { pool } from "../config/db";
 
 export const getAllDestinationsRepo = async () => {
-    const result = await pool.query(`SELECT * FROM destination`);
+    const result = await pool.query(`SELECT * FROM destination ORDER BY name ASC`);
     return result.rows;
 };
 
-export const createDestinationRepo = async (name: string) => {
+export const createDestinationRepo = async (name: string, location: string, description: string) => {
     const result = await pool.query(
-        `INSERT INTO destination (name) VALUES ($1) RETURNING *`,
-        [name]
+        `INSERT INTO destination (name, location, description) VALUES ($1, $2, $3) RETURNING *`,
+        [name, location, description]
     );
     return result.rows[0];
 };
