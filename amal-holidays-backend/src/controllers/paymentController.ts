eb0b76dc3a940fc64db1a37b4088ecffbb8dc42f
Thineth <thinethgeewinda660@gmail.com>
2026-04-21 13:49:01 +0530

    feat(amal-holidays-backend/src/controllers): implement the request and reponse handling logic for the payment

---
+++ amal-holidays-backend/src/controllers/paymentController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { createPayment } from "../services/paymentService";
+
+export const createPaymentController = async (req: any, res: Response) => {
+    try {
+        const payment = await createPayment(req.body);
+
+        res.status(201).json({ message: "Payment successful", payment, });
+    } catch (error: any) {
+        res.status(400).json({ message: error.message });
+    };
+}
