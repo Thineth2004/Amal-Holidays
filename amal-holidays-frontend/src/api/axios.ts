ce09c680a68bad66c5e85a68a6193d176a535654
Thineth <thinethgeewinda660@gmail.com>
2026-04-23 20:12:47 +0530

    refactor(amal-holidays-frontend/src/api): put the api folder inside the src folder

--- amal-holidays-frontend/src/api/axios.ts
+++ amal-holidays-frontend/src/api/axios.ts
@@ -1,4 +1,4 @@
-import axios, { InternalAxiosRequestConfig } from "axios";
+import axios, { type InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
     baseURL: "http://localhost:5000/api",
