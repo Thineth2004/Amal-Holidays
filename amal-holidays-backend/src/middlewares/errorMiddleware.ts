e1fd66c4dc2f0b5475c09118fe54a259f55e2200
Thineth <thinethgeewinda660@gmail.com>
2026-04-23 16:01:06 +0530

    feat(amal-holidays-backend/src/middlewares): update the error middleware to sort the erros as operational and non-operational

--- amal-holidays-backend/src/middlewares/errorMiddleware.ts
+++ amal-holidays-backend/src/middlewares/errorMiddleware.ts
@@ -7,13 +7,28 @@ export const globalErrorHandler = (
   next: NextFunction
 ) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+  err.status = '${err.statusCode}'.startsWith('4') ? 'fail': 'error';
 
-  console.error("ERROR:", err);
+  if (process.env.NODE_ENV === "development") {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  } else {
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    } else {
+      console.error("NON-OPERATIONAL ERROR:", err);
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message || "Something went wrong on the server",
-    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
-  });
+      res.status(500).json({
+        status: "error",
+        message: "Oops...Something went wrong!"
+      });
+    }
+  }
 };
