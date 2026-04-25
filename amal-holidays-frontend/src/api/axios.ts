23d6266b28a7880b71e296840eb0a5941fe29919
Thineth <thinethgeewinda660@gmail.com>
2026-04-25 14:31:26 +0530

    feat(amal-holidays-frontend/src/api): add the response handling interceptor to the axios

--- amal-holidays-frontend/src/api/axios.ts
+++ amal-holidays-frontend/src/api/axios.ts
@@ -4,6 +4,27 @@ const api = axios.create({
     baseURL: "http://localhost:5000/api",
 });
 
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        console.log("Interceptor caught an error!", error.response?.status);
+
+        if (error.response?.status === 401) {
+            alert("Session expired. Please login again.");
+            localStorage.clear();
+            window.location.href = "/";
+        }
+
+        if (error.response?.status === 500) {
+            alert("The server is having a bad day. Try again later.");
+        }
+
+        return Promise.reject(error);
+    }
+)
+
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     const token = localStorage.getItem("token");
     if (token && config.headers) {
