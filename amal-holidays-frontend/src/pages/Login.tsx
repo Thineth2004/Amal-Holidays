6ff03d96a5493f6a92e7c8a7b8815553bfce9f7d
Thineth <thinethgeewinda660@gmail.com>
2026-04-25 14:50:39 +0530

    feat(amal-holidays-frontend/src/pages): add the handle login function with try-catch blocks to handle the incoming errors from the interceptor

---
+++ amal-holidays-frontend/src/pages/Login.tsx
@@ -0,0 +1,24 @@
+import api from "../api/axios";
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const res = await api.post("auth/login", { email, password });
+            login(res.data);
+            navigate("/dashboard");
+        } catch (err: any) {
+            if (err.response?.status === 401) {
+                setError("Invalid Email or Password. Please check again.");
+            }
+        }
+    };
+}
