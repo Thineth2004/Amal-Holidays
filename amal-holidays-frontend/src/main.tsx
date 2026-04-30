b57c8ae54f8870747f608accc078b9d403a42389
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-04-30 22:16:00 +0530

    feat: wrap application with AuthProvider

--- amal-holidays-frontend/src/main.tsx
+++ amal-holidays-frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <App />,
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
 )
