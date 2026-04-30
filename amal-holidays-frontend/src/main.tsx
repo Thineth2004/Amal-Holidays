c97da6ad2f0d721965f194283a0d3453043513bf
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-04-30 22:47:22 +0530

    update main.tsx

--- amal-holidays-frontend/src/main.tsx
+++ amal-holidays-frontend/src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './contexts/AuthProvider'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
     <App />
+    <Toaster />
   </AuthProvider>,
 )
