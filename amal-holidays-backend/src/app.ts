ca94254a3e428000501055054c3add6afb519352
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-01 15:15:10 +0530

    update app.ts

--- amal-holidays-backend/src/app.ts
+++ amal-holidays-backend/src/app.ts
@@ -13,10 +13,10 @@ import path from "path";
 
 const app = express();
 
-// Ensure uploads directory exists
+/* Ensure uploads directory exists */
 const uploadDir = path.join(__dirname, "../uploads");
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
+  fs.mkdirSync(uploadDir);
 }
 
 app.use(cors());
