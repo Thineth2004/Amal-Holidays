767ae0537c2b11ead6e771bf54d23b58629e6233
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-06 16:21:16 +0530

    feat: create axiosInstance.ts

---
+++ amal-holidays-admin/src/api/axiosInstance.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { backend_url } from '../config/config';
+
+const api = axios.create({
+  baseURL: backend_url,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
