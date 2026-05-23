66cbc6ea0b2b8db8a3b36294b276c55e6e81efd6
Thineth <thinethgeewinda660@gmail.com>
2026-05-23 10:00:19 +0530

    feat: chnage the safe user to user

--- amal-holidays-backend/src/controllers/authController.ts
+++ amal-holidays-backend/src/controllers/authController.ts
@@ -27,7 +27,7 @@ export const login = async (req: Request, res: Response) => {
         res.json({
             message: "Login successful",
             token,
-            safeUser,
+            user: safeUser, // <--- CHANGE safeUser TO user HERE
         });
     } catch (error: any) {
         res.status(400).json({ message: error.message });
