d1a632553b3716e2676e3b1a97bcf887205d1e21
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-23 15:22:13 +0530

    update paymentController.ts

--- amal-holidays-backend/src/controllers/paymentController.ts
+++ amal-holidays-backend/src/controllers/paymentController.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
 import { createPayment } from "../services/paymentService";
+import { getAllPaymentsRepo } from "../repositories/paymentRepository";
+
+export const getAllPaymentsController = async (req: any, res: Response) => {
+    try {
+        const payments = await getAllPaymentsRepo();
+        res.status(200).json({
+            success: true,
+            data: payments
+        });
+    } catch (error: any) {
+        res.status(400).json({ message: error.message });
+    }
+};
 
 export const createPaymentController = async (req: any, res: Response) => {
     try {
