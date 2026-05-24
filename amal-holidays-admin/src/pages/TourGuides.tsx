c5af3f682a2f7f6a9472c316af20eafe22e00518
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-24 23:05:24 +0530

    feat: add TourGuides page component for managing tour guide details and bookings

---
+++ amal-holidays-admin/src/pages/TourGuides.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StaffPage from '../components/StaffPage';
+
+const TourGuides: React.FC = () => {
+  return <StaffPage role="Guide" title="Tour Guide" endpoint="tour-guides" />;
+};
+
+export default TourGuides;
