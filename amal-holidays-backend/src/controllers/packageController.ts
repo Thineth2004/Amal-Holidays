a8ef37d5de80c45fab954b2a6f499ec85cd3c534
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-24 10:51:06 +0530

    update packageController.ts

--- amal-holidays-backend/src/controllers/packageController.ts
+++ amal-holidays-backend/src/controllers/packageController.ts
@@ -1,16 +1,10 @@
 import { Request, Response } from "express";
-import {
-    createPackage,
-    getAllPackages,
-    getAvailablePackages,
-    getPackageById,
-    updatePackage,
-} from "../services/packageService";
+import * as service from "../services/packageService";
 
 // Manager 
 export const createPackageController =  async (req: Request, res: Response) => {
     try {
-        const pkg = await createPackage(req.body);
+        const pkg = await service.createPackage(req.body);
         res.status(201).json(pkg);
     } catch (error: any) {
         res.status(400).json({ message: error.message });
@@ -19,29 +13,38 @@ export const createPackageController =  async (req: Request, res: Response) => {
 
 // Manager
 export const getAllPackagesController = async (req: Request, res: Response) => {
-    const packages = await getAllPackages();
+    const packages = await service.getAllPackages();
     res.json(packages);
 };
 
 // All users
 export const getAvailablePackagesController = async (req: Request, res: Response) => {
-    const packages = await getAvailablePackages();
+    const packages = await service.getAvailablePackages();
     res.json(packages);
 };
 
 // Get single
 export const getPackageController = async (req: Request, res: Response) => {
-    const pkg = await getPackageById(Number(req.params.id));
+    const pkg = await service.getPackageById(Number(req.params.id));
     res.json(pkg);
 };
 
 // Manager
 export const updatePackageController = async (req: Request, res: Response) => {
     try {
-        const pkg = await updatePackage(Number(req.params.id), req.body);
+        const pkg = await service.updatePackage(Number(req.params.id), req.body);
         res.json(pkg);
     } catch (error: any) {
         res.status(400).json({ message: error.message });
     }
 };
 
+// Manager
+export const deletePackageController = async (req: Request, res: Response) => {
+    try {
+        const deletedPkg = await service.deletePackage(Number(req.params.id));
+        res.json({ message: "Package deleted successfully", package: deletedPkg });
+    } catch (error: any) {
+        res.status(400).json({ message: error.message });
+    }
+};
