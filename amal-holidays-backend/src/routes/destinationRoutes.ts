b95fc6ff6631f88d964d6abbf8a31e60f3144800
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-24 10:51:23 +0530

    update destinationRoutes.ts

--- amal-holidays-backend/src/routes/destinationRoutes.ts
+++ amal-holidays-backend/src/routes/destinationRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getDestinations, createDestination } from "../controllers/destinationController";
+import { getDestinations, createDestination, updateDestination, deleteDestination } from "../controllers/destinationController";
 import { authenticate } from "../middlewares/authMiddleware";
 import { authorize } from "../middlewares/roleMiddleware";
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.get("/", getDestinations);
 router.post("/", authenticate, authorize("Manager"), createDestination);
+router.put("/:id", authenticate, authorize("Manager"), updateDestination);
+router.delete("/:id", authenticate, authorize("Manager"), deleteDestination);
 
 export default router;
