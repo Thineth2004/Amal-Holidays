a988bb583662dcbe51863bf9fa0777cee5839919
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-24 23:04:52 +0530

    feat: add Drivers page component to manage driver details and bookings

---
+++ amal-holidays-admin/src/pages/Drivers.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StaffPage from '../components/StaffPage';
+
+const Drivers: React.FC = () => {
+  return <StaffPage role="Driver" title="Driver" endpoint="drivers" />;
+};
+
+export default Drivers;
