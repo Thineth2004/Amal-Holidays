1311f44dd4b2f922eaa82f1438972829e02c0d68
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-24 10:51:33 +0530

    update destinationService.ts

--- amal-holidays-backend/src/services/destinationService.ts
+++ amal-holidays-backend/src/services/destinationService.ts
@@ -1,18 +1,34 @@
 import * as repo from "../repositories/destinationRepository";
 
 export const fetchAllDestinations = async () => {
-  // Add any business logic or data transforms here if needed before returning to controller
   return await repo.getAllDestinationsRepo();
 };
 
 export const addNewDestination = async (
-  name: string, 
-  location: string, 
-  description: string, 
-  imageUrl: string
+  name: string,
+  location: string,
+  description: string,
+  image_uuid: string
 ) => {
   if (!name || !location) {
     throw new Error("Name and location fields are required.");
   }
-  return await repo.createDestinationRepo(name, location, description, imageUrl);
+  return await repo.createDestinationRepo(name, location, description, image_uuid);
+};
+
+export const updateDestination = async (
+  id: number,
+  name: string,
+  location: string,
+  description: string,
+  image_uuid?: string
+) => {
+  if (!name || !location) {
+    throw new Error("Name and location fields are required.");
+  }
+  return await repo.updateDestinationRepo(id, name, location, description, image_uuid);
+};
+
+export const deleteDestination = async (id: number) => {
+  return await repo.deleteDestinationRepo(id);
 };
