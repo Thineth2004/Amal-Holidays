54e45cf0cb60c3a0e1fccf6a202b3885ad974e26
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-24 20:42:50 +0530

    update bookingRoutes.ts

--- amal-holidays-backend/src/routes/bookingRoutes.ts
+++ amal-holidays-backend/src/routes/bookingRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBookingController, getAllBookingsController } from "../controllers/bookingController";
+import { createBookingController, getAllBookingsController, deleteBookingController } from "../controllers/bookingController";
 import { authenticate } from "../middlewares/authMiddleware";
 import { authorize } from "../middlewares/roleMiddleware";
 import { validateBookingInput } from "../middlewares/validateMiddleware";
@@ -21,6 +21,14 @@ router.post(
   authorize("Tourist"),
   validateBookingInput,
   createBookingController
+);
+
+// Manager/Staff only - Delete a booking (restore package slots)
+router.delete(
+  "/:id",
+  authenticate,
+  authorize("Manager", "Staff"),
+  deleteBookingController
 );
 
 export default router;
