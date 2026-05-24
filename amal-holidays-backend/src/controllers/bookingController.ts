aa75956010fe4503323d8a88cbd2494362b9bede
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-05-24 20:42:39 +0530

    update bookingController.ts

--- amal-holidays-backend/src/controllers/bookingController.ts
+++ amal-holidays-backend/src/controllers/bookingController.ts
@@ -1,5 +1,5 @@
 import { Response } from "express";
-import { createBooking } from "../services/bookingService";
+import { createBooking, deleteBooking } from "../services/bookingService";
 import { getAllBookingsRepo } from "../repositories/bookingRepository";
 import { catchAsync } from "../utils/catchAsync"; 
 import { AppError } from "../utils/AppError";
@@ -32,5 +32,20 @@ export const createBookingController = catchAsync(async (req: any, res: Response
         success: true,
         message: "Booking confirmed successfully!",
         data: booking
+    });
+});
+
+export const deleteBookingController = catchAsync(async (req: any, res: Response) => {
+    const bookingId = Number(req.params.id);
+
+    if (isNaN(bookingId)) {
+        throw new AppError('Invalid booking id', 400);
+    }
+
+    await deleteBooking(bookingId);
+
+    res.status(200).json({
+        success: true,
+        message: 'Booking deleted successfully'
     });
 });
